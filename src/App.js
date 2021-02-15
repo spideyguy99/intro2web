06f3475f3d2418be715d2e71ee9563363872839d
Noah Furniss <57143709+spideyguy99@users.noreply.github.com>
2021-02-14 22:22:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+        <h3>My name is Noah Furniss </h3>
+        <h5>Concentration: web developement in CIT</h5>
+        <p>My favorite color is: <span className="colorFav"> red</span></p>
+
+      </div>
   );
 }
 
